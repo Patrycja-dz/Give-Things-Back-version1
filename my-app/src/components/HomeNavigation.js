bdb9ba532d11aa6919b4ac6173d0fde306d9a0d7
Patrycja Dziemianko <patrycja.dziemainko@wp.pl>
2019-09-11 17:27:58 +0200

    part of task 1b

--- my-app/src/components/HomeNavigation.js
+++ my-app/src/components/HomeNavigation.js
@@ -1,14 +1,17 @@
 import React from "react"
 import {NavLink} from "react-router-dom";
 
-class HomeNavigation extends React.Component{
+class HomeNavigation extends React.Component {
     render() {
-        return(
-         <div>
-             <NavLink to="/">Home</NavLink>
-             <NavLink to="/login">Zaloguj</NavLink>
-             <NavLink to="/register">Załóż konto</NavLink>
-         </div>
+        return (
+
+                <div className="firstHeader">
+                    <NavLink id="hideHome" to="/">Home</NavLink>
+                    <NavLink  id="routeLinks" className="regist" to="/register"> Załóż konto </NavLink>
+                    <NavLink id="routeLinks" className="login" to="/login">Zaloguj </NavLink>
+
+                </div>
+
         );
     }
 }
--- my-app/src/components/HomeSecondSection.js
+++ my-app/src/components/HomeSecondSection.js
@@ -2,10 +2,36 @@ import React from "react";
 
 class HomeSecondSection extends React.Component {
     render() {
-        return(
-            <div>
-                <p>tu bedzie foto</p>
+        return (
+            <div className="main-div">
+                <div className="first_img"></div>
+                <div className="nav-div">
+                    <nav>
+                        <ul className="nav-links">
+                            <li id="start-link">Start</li>
+                            <li>O co chodzi</li>
+                            <li>O nas</li>
+                            <li>Fundacje i organizacje</li>
+                            <li>Kontakt</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="heading-div">
+                    <p className="first-p"> Zacznij pomagać !</p>
+                    <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+
+                <div className="div-for-btn">
+                    <div className="first-btn">
+                        <p>Oddaj <br/> rzeczy</p>
+                    </div>
+                    <div className="second-btn">
+                        <p>Zorganizuj<br/> zbiórkę</p>
+                    </div>
+                </div>
+                </div>
+
             </div>
+
         );
     }
 }
