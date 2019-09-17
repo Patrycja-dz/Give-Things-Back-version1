e72d26ba3a3cea8d33b0437c7597eceb094dc2ff
Patrycja Dziemianko <patrycja.dziemainko@wp.pl>
2019-09-17 21:22:23 +0200

    login form

--- my-app/src/components/HomeNavigation.js
+++ my-app/src/components/HomeNavigation.js
@@ -3,6 +3,14 @@ import {NavLink} from "react-router-dom";
 // import Nav from "./Nav";
 import {Link} from "react-scroll";
 
+const options = {
+    activeClass: "active",
+    spy: true,
+    smooth: true,
+    offset: 50,
+    duration: 500
+}
+
 class HomeNavigation extends React.Component {
     render() {
         return (
@@ -17,10 +25,10 @@ class HomeNavigation extends React.Component {
                     <nav>
                         <ul className="nav-links">
                             <li id="start-link"><NavLink id="special-link" to="/">Start</NavLink></li>
-                            <li><Link to="onlyFourSteps">O co chodzi</Link></li>
-                            <li><Link to="aboutUs">O nas</Link></li>
-                            <li><Link to="whomWeHelp"> Fundacje i organizacje</Link></li>
-                            <li><Link to="contact"> Kontakt</Link></li>
+                            <li><Link to="onlyFourSteps" {...options}>O co chodzi</Link></li>
+                            <li><Link to="aboutUs" {...options}>O nas</Link></li>
+                            <li><Link to="whomWeHelp" {...options}> Fundacje i organizacje</Link></li>
+                            <li><Link to="contact" {...options}> Kontakt</Link></li>
                         </ul>
                     </nav>
                 </div>
--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,11 +1,70 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 class Login extends React.Component {
+    state = {
+        email: "",
+        pass: "",
+        errors: [],
+
+    };
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        const errors = [];
+        if (this.state.email.indexOf("@") === -1) {
+            errors.push('Podany email jest nieprawidłowy!')
+        }
+        if (this.state.pass.length <= 6) {
+            errors.push('Podane hasło jest za krótkie');
+        }
+        this.setState({
+            errors: errors,
+        })
+
+    };
+
     render() {
-        return(
-            <div>
-                <p>Login Page</p>
+        const erorrsMessages = (
+            <div className="error_div">
+                {this.state.errors.map((err, index) => <p key={index}>{err}</p>)}
             </div>
+        );
+        return (
+            <div>
+                <p className="log_in">Zaloguj się</p>
+                <div className="image"></div>
+                <div className="form_content">
+                    <div className="login_form" noValidate>
+                        {erorrsMessages}
+                        <label><span>Email</span></label>
+                        <input onChange={this.handleChange} name="email" value={this.state.email} type="email"
+                               autoComplete="off"/>
+                        <label className="password"><span>Hasło</span></label>
+                        <input onChange={this.handleChange} name="pass" value={this.state.pass}
+                               type="password"/>
+
+                    </div>
+
+
+                </div>
+
+
+                <div className="login_form_btn">
+                    <ul className="login_form_div">
+                        <button className="submit">Założ konto</button>
+                        <button className="log_btn" onClick={this.handleSubmit}><NavLink to="/register">Zaloguj
+                            się</NavLink></button>
+
+                    </ul>
+                </div>
+            </div>
+
+
         );
     }
 }
